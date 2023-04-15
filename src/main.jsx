8e8e61cd168a815e761135dbb1f5c7eba4c95ef0
alitolibov <alitolibov066@gmail.com>
2023-04-16 00:32:58 +0500

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import './index.css'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from "react-router-dom"
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <React.StrictMode>
+    <ChakraProvider>
     <Provider store={store}>
     <App />
     </Provider>
+    </ChakraProvider>
   </React.StrictMode>
   </BrowserRouter>
 )
