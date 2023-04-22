f6ae6db0d1e24f65235555d2008ecb309a5f79b0
alitolibov <alitolibov066@gmail.com>
2023-04-22 23:01:36 +0500

    fixed bugs

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,21 +1,25 @@
 import { Key } from "@mui/icons-material";
 import { useContext, useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
 import { useSwiper } from "swiper/react";
 import Img from "../components/Img";
 import SwiperProduct from "../components/SwiperProduct";
 import SwiperProductMobile from "../components/SwiperProductMobile";
 import { cursorCTX } from "../context/cursorCTX";
+import { addLiked, removeLiked } from "../features/liked/likedSlice";
 
 
 const ProductPage = () => {
     let {cursor} = useContext(cursorCTX)
+    let dispatch = useDispatch()
+    const likedID = useSelector(state => state.liked.data_id)
     let data = useSelector(state => state.goods.data)
     let {pathname} = useLocation()
     let ids = pathname.split('/').at(-1)
     const [count, setCount] = useState(1)
     let filtered = data.filter(item => item.id === +ids)
+    let itemProduct = filtered[0]
     let {title, type, rating, salePercentage, price, id, description, media} = filtered[0]
     function prettify(num) {
         var n = num.toString();
@@ -127,7 +131,14 @@ const ProductPage = () => {
                         <img data-v-b3e9397c="" src="data:image/svg+xml,%3csvg width='18' height='18' viewBox='0 0 19 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3e %3cpath d='M9 12.9525L13.635 15.75L12.405 10.4775L16.5 6.93L11.1075 6.4725L9 1.5L6.8925 6.4725L1.5 6.93L5.595 10.4775L4.365 15.75L9 12.9525Z' fill='%23F5A623'/%3e %3c/svg%3e" data-test-id="icon__rating-star" style={{width: '14px'}}/>
                         <p className="text-[13px] text-[#8a8d93]">{rating}</p>
                     </div>
-                    <img src="../../public/images/heart.png" className="w-[20px] duration-[300ms] h-[20px] hover:invert-[70%]" alt="" />
+                    {
+                likedID.includes(id) ? <svg onClick={() => dispatch(removeLiked(itemProduct))} className="cursor-pointer w-fit float-right" data-v-b3e9397c="" width="20" height="20" viewBox="0 0 19 16" fill="none" xmlns="http://www.w3.org/2000/svg" alt="like">
+                <path d="M5.45 0.169434C8.01792 0.169434 9.5 2.32178 9.5 2.32178C9.5 2.32178 10.985 0.169434 13.55 0.169434C16.205 0.169434 18.5 2.23943 18.5 5.11943C18.5 9.34995 12.0604 13.7892 9.86509 15.7297C9.65819 15.9126 9.34179 15.9126 9.13488 15.7297C6.94056 13.7903 0.5 9.34976 0.5 5.11943C0.5 2.23943 2.795 0.169434 5.45 0.169434Z" fill="#8967F0"></path>
+                </svg> : <svg onClick={() => dispatch(addLiked(itemProduct))} className="cursor-pointer w-fit float-right top-[5px] right-[5px]" data-v-b3e9397c="" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5.95 2C8.51792 2 10 4.15234 10 4.15234C10 4.15234 11.485 2 14.05 2C16.705 2 19 4.07 19 6.95C19 11.1805 12.5604 15.6197 10.3651 17.5603C10.1582 17.7432 9.84179 17.7432 9.63488 17.5603C7.44056 15.6209 1 11.1803 1 6.95C1 4.07 3.295 2 5.95 2Z" fill="white" fillOpacity="0.8"></path>
+                <path d="M1 6.86486C1 4.20297 3.15017 2 5.86486 2C7.98685 2 9.35921 3.35876 10 4.18673C10.6408 3.35876 12.0132 2 14.1351 2C16.8506 2 19 4.20302 19 6.86486C19 8.02987 18.5328 9.18622 17.8534 10.265C17.1716 11.3476 16.252 12.3903 15.29 13.3377C13.9567 14.6508 12.4757 15.8387 11.4134 16.6907C10.9618 17.0529 10.5859 17.3544 10.3293 17.579C10.1407 17.7439 9.85926 17.7439 9.67075 17.579C9.41405 17.3544 9.03815 17.0529 8.58659 16.6907C7.52431 15.8387 6.04326 14.6508 4.70997 13.3377C3.74802 12.3903 2.82836 11.3476 2.14659 10.265C1.46724 9.18622 1 8.02987 1 6.86486ZM5.86486 3C3.70929 3 2 4.74838 2 6.86486C2 7.76743 2.36553 8.73607 2.99277 9.73208C3.61759 10.7242 4.47833 11.706 5.41165 12.6252C6.71033 13.9042 8.08423 15.005 9.13396 15.8461C9.45728 16.1052 9.74985 16.3396 10 16.547C10.2501 16.3396 10.5427 16.1052 10.866 15.8461C11.9158 15.005 13.2897 13.9042 14.5883 12.6252C15.5217 11.706 16.3824 10.7242 17.0072 9.73208C17.6345 8.73607 18 7.76743 18 6.86486C18 4.74833 16.2914 3 14.1351 3C12.0406 3 10.8181 4.70211 10.5033 5.21028C10.2727 5.5825 9.72727 5.58249 9.4967 5.21027C9.1819 4.7021 7.95944 3 5.86486 3Z" fill="#15151A"></path>
+                </svg>
+            }
                 </div>
                 <p className="text-[22px] text-[#212121]">{title}</p>
                 <div className="flex items-center gap-x-[16px] h-[28px]">
