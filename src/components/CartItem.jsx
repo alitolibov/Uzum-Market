9abb8d87771545ddc205ecefd15e0f4acbb13625
Ali-web-developer <109537573+alitolibov@users.noreply.github.com>
2023-05-04 21:56:00 +0500

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
 import { decrement, increment, removeGood } from "../features/cart/cartSlice"
 
 const CartItem = ({item}) => {
