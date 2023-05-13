57e24ea96b8e76743e092d555172e63a54a3721d
Ali-web-developer <109537573+alitolibov@users.noreply.github.com>
2023-05-13 23:42:07 +0500

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartSlice from '../features/cart/cartSlice'
-import goodsSlice  from '../features/goods/goodsSlice'
+import goodsSlice  from '../features/goods/goodsSlice';
 import likedSlice from '../features/liked/likedSlice';
 
 export const store = configureStore({
